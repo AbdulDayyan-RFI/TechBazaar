1bcc5fafdc3e811f6b34a13d59a3699bacfae02d
AbdulDayyan-RFI <mabduldayyan@gmail.com>
2017-08-07 23:50:44 +0530

    TechBazaar Solution Check In

---
+++ TechBazaar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechBazaar.Startup))]
+namespace TechBazaar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
